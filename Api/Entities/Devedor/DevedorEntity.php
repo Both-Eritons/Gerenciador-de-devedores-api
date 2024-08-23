27411e8406eb107a0f979ccfd90f3187bcf29d66
Eriton <botheritons2005@gmail.com>
2024-08-23 14:36:51 -0300

    feat: Getters and Setters function to Devedor Entity

--- Api/Entities/Devedor/DevedorEntity.php
+++ Api/Entities/Devedor/DevedorEntity.php
@@ -3,19 +3,20 @@
 namespace Api\Entities\Devedor;
 
 use Api\Models\Devedor\DevedorModel;
-use App\Exception\Entity\EntityPropertyNotFound;
 use DateTime;
 
-class DevedorEntity {
+class DevedorEntity
+{
 
-  private ?int $id;
+  private ?int $id = null;
   private string $nome;
-  private string $email;
-  private string $telefone;
-  private string $apelido;
-  private ?DateTime $dataRegistro;
+  private ?string $email = null;
+  private ?string $telefone = null;
+  private ?string $apelido = null;
+  private ?DateTime $dataRegistro = null;
 
-  public function __construct(DevedorModel $devedor) {
+  public function __construct(DevedorModel $devedor)
+  {
     $this->id = $devedor->id;
     $this->nome = $devedor->nome;
     $this->email = $devedor->email;
@@ -24,12 +25,72 @@ class DevedorEntity {
     $this->dataRegistro = $devedor->dataRegistro;
   }
 
-  function __get(string $name)
+  function getId(): ?int
   {
-    if(!isset($this->$name)) {
-     throw new EntityPropertyNotFound();
-    }
+    return $this->id;
   }
 
-  
+  function getNome(): ?string
+  {
+    return $this->nome;
+  }
+  function getEmail(): ?string
+  {
+    return $this->email;
+  }
+  function getTelefone(): ?string
+  {
+    return $this->telefone;
+  }
+  function getApelido(): ?string
+  {
+    return $this->apelido;
+  }
+  function getDataRegistro(): ?DateTime
+  {
+    return $this->dataRegistro;
+  }
+
+  function setId(int $id): self
+  {
+    $this->id = $id;
+    return $this;
+  }
+  function setNome(string $nome): self
+  {
+    $this->nome = $nome;
+    return $this;
+  }
+  function setEmail(string $email): self
+  {
+    $this->email = $email;
+    return $this;
+  }
+  function setTelefone(string $telefone): self
+  {
+    $this->telefone = $telefone;
+    return $this;
+  }
+  function setApelido(string $apelido): self
+  {
+    $this->apelido = $apelido;
+    return $this;
+  }
+  function setDataRegistro(string $data): self
+  {
+    $this->dataRegistro = $data;
+    return $this;
+  }
+
+  function toArray(): array{
+    $array = array(
+      "id" => $this->getId(),
+      "nome" => $this->getNome(),
+      "email" => $this->getEmail(),
+      "telefone" => $this->getTelefone(),
+      "apelido" => $this->getApelido(),
+      "registro" => $this->getDataRegistro()
+    );
+    return $array;
+  }
 }
