030ac3ec8539c9eb44a10439dc60b6302e1c82d6
Eriton <botheritons2005@gmail.com>
2024-08-23 16:28:51 -0300

    feat: Devedor Constant Messages

---
+++ App/Constant/Devedor/Messages.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Constant\Devedor;
+
+class Messages {
+  static const CREATE_SUCCESS = "Sucesso ao Criar Devedor.";
+  static const CREATE_ERROR = "Ocorreu algum Erro ao Criar Devedor.";
+  static const UPDATE_SUCCESS = "Sucesso ao Atualizar Devedor.";
+}
