0708072c99b74d42cf6be7e7e64f00abe72a47fa
Eriton <botheritons2005@gmail.com>
2024-08-24 11:22:25 -0300

    feat: add more http code

--- App/Constant/Http/Code.php
+++ App/Constant/Http/Code.php
@@ -3,6 +3,14 @@
 namespace App\Constant\Http;
 
 class Code {
-  const int OK = 200;
-  const int CREATED = 202;
+  const int OK = 200,
+    CREATED = 201,
+    ACCEPTED = 202,
+    BAD = 400,
+    UNAUTHORIZED = 401,
+    PAYMENT_REQUIRED= 402,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    SERVER_ERROR = 500,
+    SERVICE_UNAVAILABLE = 503;
 }
